302f7e8974a59103133b57cbec22c38bd4bc0b70
msssharma <shwetasharmaa2022@gmail.com>
2023-07-13 15:10:33 +0100

    counting even numbers

---
+++ src/CountingEvenInts.java
@@ -0,0 +1,28 @@
+public class CountingEvenInts {
+    public static void main(String[] args) {
+        int a[] = {2,1,2,3,4};
+        for (int i = 0; i<a.length; i++){
+            if (a[i]%2==0)
+            System.out.println(a[i]);
+        }
+
+    }
+}
+class CountEvenInArray{
+    public static void main(String[] args) {
+        int b[] = {2,2,0};
+        for (int j = 0; j<b.length; j++){
+            if (b[j]%2==0)
+                System.out.println(b[j]);
+
+        }
+
+
+
+
+        int c[] = {1,3,5};
+        for (int k =0; k<c.length; k++){
+            if (c[k] % 2 ==0)
+    System.out.println(c[k]);
+    }
+}}
